e14da9b5306029b5b6abd42432c410d4c6903f90
Renato Siqueira <renatoelysiqueira@gmail.com>
2020-08-30 17:50:43 -0300

    fix base64

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,4 +1,5 @@
 import { GoogleSpreadsheet } from "google-spreadsheet";
+import fromBase64 from "../../lib/base64";
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID);
 
@@ -6,7 +7,7 @@ export default async (req, res) => {
   try {
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
-      private_key: process.env.SHEET_PRIVATE_KEY,
+      private_key: fromBase64(process.env.SHEET_PRIVATE_KEY),
     });
     await doc.loadInfo();
 
--- pages/api/save.js
+++ pages/api/save.js
@@ -1,4 +1,5 @@
 import { GoogleSpreadsheet } from "google-spreadsheet";
+import fromBase64 from "../../lib/base64";
 import moment from "moment";
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID);
@@ -14,7 +15,7 @@ export default async (req, res) => {
   try {
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
-      private_key: process.env.SHEET_PRIVATE_KEY,
+      private_key: fromBase64(process.env.SHEET_PRIVATE_KEY),
     });
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[1];
