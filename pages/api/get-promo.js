8f082974423cd4f2f342a0dd948f3f4364ab921a
Renato Siqueira <renatoelysiqueira@gmail.com>
2020-08-30 17:52:49 -0300

    fix base64

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,5 +1,5 @@
 import { GoogleSpreadsheet } from "google-spreadsheet";
-import fromBase64 from "../../lib/base64";
+import { fromBase64 } from "../../lib/base64";
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID);
 
