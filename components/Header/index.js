472a601070b4da013d868a095e9f0de3e5a2837f
Renato Siqueira <renatoelysiqueira@gmail.com>
2020-08-29 22:17:28 -0300

    css modules added

---
+++ components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const Header = () => (
+    <div className={styles.wrapper}>
+        <div className='container mx-auto'>
+            Header
+        </div>
+    </div>
+)
+
+export default Header
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import '../css/styles.css'
 
+import Header from '../components/Header'
+
 const MyApp = ({Component, pageProps}) => {
     return (
         <div>
-            <h1>MyApp</h1>
-            <Component {...pageProps} />
+            <Header/>
+            <div className='container mx-auto'>
+                <Component {...pageProps} />
+            </div>
         </div>
     )
 }
