307f2dca67e60fb74b071b043778236c318cb283
Renato Siqueira <renatoelysiqueira@gmail.com>
2020-11-24 10:23:01 -0300

    teste

--- components/Header/index.js
+++ components/Header/index.js
@@ -35,6 +35,7 @@ const Header = () => (
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
+          <div className={styles.divSocial}>TESTE</div>
           <span className="ml-3 text-xl">NOME DO RESTAURANTE</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
